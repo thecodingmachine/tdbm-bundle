07bb75c0e2c580ad6a356bb292471885a80ca5b3
David Négrier <d.negrier@thecodingmachine.com>
2019-03-15 12:04:22 +0100

    Adding unit tests

---
+++ Tests/FunctionalTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TheCodingMachine\TDBM\Bundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use TheCodingMachine\GraphQLite\Schema;
+use TheCodingMachine\TDBM\TDBMService;
+
+class FunctionalTest extends TestCase
+{
+    public function testServiceWiring()
+    {
+        $kernel = new TdbmTestingKernel('test', true);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        $tdbmService = $container->get(TDBMService::class);
+        $this->assertInstanceOf(TDBMService::class, $tdbmService);
+    }
+}
