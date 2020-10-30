411e1cc1982078e876ca311c1ff2b1df59ee4a15
David Négrier <d.negrier@thecodingmachine.com>
2020-10-30 15:35:58 +0100

    Adding end to end unit tests

--- Tests/FunctionalTest.php
+++ Tests/FunctionalTest.php
@@ -2,10 +2,15 @@
 
 namespace TheCodingMachine\TDBM\Bundle\Tests;
 
+use Doctrine\DBAL\Connection;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Definition\Processor;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\ApplicationTester;
+use TheCodingMachine\FluidSchema\TdbmFluidSchema;
 use TheCodingMachine\TDBM\Bundle\DependencyInjection\Configuration;
 use TheCodingMachine\TDBM\TDBMService;
+use Throwable;
 
 class FunctionalTest extends TestCase
 {
@@ -71,5 +76,76 @@ class FunctionalTest extends TestCase
         $expected['naming']['exceptions'] = ['object' => 'AnObject'];
 
         $this->assertEquals($expected, $processor->processConfiguration($configuration, [['naming' => ['exceptions' => ['object' => 'AnObject']]]]));
+    }
+
+    public function testEndToEnd(): void
+    {
+        $kernel = new TdbmTestingKernel(true);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        /**
+         * @var Connection $connectionRoot
+         */
+        $connectionRoot = $container->get('doctrine.dbal.root_connection');
+
+        $connectionRoot->getSchemaManager()->dropAndCreateDatabase('test_tdbmbundle');
+        $connectionRoot->getSchemaManager()->dropAndCreateDatabase('test_tdbmbundle2');
+
+        /**
+         * @var Connection $connection1
+         */
+        $connection1 = $container->get('doctrine.dbal.default_connection');
+
+        $fromSchema1 = $connection1->getSchemaManager()->createSchema();
+        $toSchema1 = clone $fromSchema1;
+
+        $db = new TdbmFluidSchema($toSchema1, new \TheCodingMachine\FluidSchema\DefaultNamingStrategy($connection1->getDatabasePlatform()));
+
+        $db->table('country')
+            ->column('id')->integer()->primaryKey()->autoIncrement()->comment('@Autoincrement')
+            ->column('label')->string(255)->unique();
+
+        $sqlStmts = $toSchema1->getMigrateFromSql($fromSchema1, $connection1->getDatabasePlatform());
+
+        foreach ($sqlStmts as $sqlStmt) {
+            //echo $sqlStmt."\n";
+            $connection1->executeStatement($sqlStmt);
+        }
+
+
+        /**
+         * @var Connection $connection2
+         */
+        $connection2 = $container->get('doctrine.dbal.other_connection');
+
+        $fromSchema2 = $connection2->getSchemaManager()->createSchema();
+        $toSchema2 = clone $fromSchema2;
+
+        $db = new TdbmFluidSchema($toSchema2, new \TheCodingMachine\FluidSchema\DefaultNamingStrategy($connection2->getDatabasePlatform()));
+
+        $db->table('person')
+            ->column('id')->integer()->primaryKey()->autoIncrement()->comment('@Autoincrement')
+            ->column('name')->string(255);
+
+        $sqlStmts = $toSchema2->getMigrateFromSql($fromSchema2, $connection2->getDatabasePlatform());
+
+        foreach ($sqlStmts as $sqlStmt) {
+            //echo $sqlStmt."\n";
+            $connection2->executeStatement($sqlStmt);
+        }
+
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $applicationTester = new ApplicationTester($application);
+        $applicationTester->run(['command' => 'tdbm:generate']);
+        $this->assertStringContainsString('Finished regenerating DAOs and beans', $applicationTester->getDisplay());
+        $this->assertFileExists(__DIR__.'/../tdbm.lock.yml');
+
+        $applicationTester = new ApplicationTester($application);
+        $applicationTester->run(['command' => 'tdbm:generate:other']);
+        $this->assertStringContainsString('Finished regenerating DAOs and beans', $applicationTester->getDisplay());
+        $this->assertFileExists(__DIR__.'/../tdbm.other.lock.yml');
     }
 }
--- Tests/TdbmTestingKernel.php
+++ Tests/TdbmTestingKernel.php
@@ -17,10 +17,15 @@ class TdbmTestingKernel extends Kernel
     use MicroKernelTrait;
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    /**
+     * @var bool
+     */
+    private $multiDb;
 
-    public function __construct()
+    public function __construct(bool $multiDb = false)
     {
         parent::__construct('test', true);
+        $this->multiDb = $multiDb;
     }
 
     public function registerBundles()
@@ -40,16 +45,58 @@ class TdbmTestingKernel extends Kernel
             ));
             $container->loadFromExtension('doctrine', array(
                 'dbal' => [
-                    'driver' => 'pdo_mysql',
-                    'server_version' => '5.7',
-                    'charset'=> 'utf8mb4',
-                    'default_table_options' => [
-                        'charset' => 'utf8mb4',
-                        'collate' => 'utf8mb4_unicode_ci',
-                    ],
-                    'url' => '%env(resolve:DATABASE_URL)%'
+                    'default_connection' => 'default',
+                    'connections' => [
+                        'default' => [
+                            'driver' => 'pdo_mysql',
+                            'server_version' => '5.7',
+                            'charset'=> 'utf8mb4',
+                            'default_table_options' => [
+                                'charset' => 'utf8mb4',
+                                'collate' => 'utf8mb4_unicode_ci',
+                            ],
+                            'url' => '%env(resolve:DATABASE_URL)%'
+                        ],
+                        'other' => [
+                            'driver' => 'pdo_mysql',
+                            'server_version' => '5.7',
+                            'charset'=> 'utf8mb4',
+                            'default_table_options' => [
+                                'charset' => 'utf8mb4',
+                                'collate' => 'utf8mb4_unicode_ci',
+                            ],
+                            'url' => '%env(resolve:DATABASE_URL2)%'
+                        ],
+                        'root' => [
+                            'driver' => 'pdo_mysql',
+                            'server_version' => '5.7',
+                            'charset'=> 'utf8mb4',
+                            'default_table_options' => [
+                                'charset' => 'utf8mb4',
+                                'collate' => 'utf8mb4_unicode_ci',
+                            ],
+                            'url' => '%env(resolve:DATABASE_URL_ROOT)%'
+                        ],
+                    ]
+
                 ]
             ));
+
+            if ($this->multiDb) {
+                $container->loadFromExtension('tdbm', array(
+                    'dao_namespace' => 'TheCodingMachine\TDBM\Bundle\Tests\GeneratedDb1\Daos',
+                    'bean_namespace' => 'TheCodingMachine\TDBM\Bundle\Tests\GeneratedDb1\Beans',
+                    'connection' => 'doctrine.dbal.default_connection',
+                    'databases' => [
+                        'other' => [
+                            'dao_namespace' => 'TheCodingMachine\TDBM\Bundle\Tests\GeneratedDb2\Daos',
+                            'bean_namespace' => 'TheCodingMachine\TDBM\Bundle\Tests\GeneratedDb2\Beans',
+                            'connection' => 'doctrine.dbal.other_connection',
+                        ]
+                    ]
+                ));
+
+            }
         });
         $confDir = $this->getProjectDir().'/Tests/Fixtures/config';
 
@@ -65,6 +112,6 @@ class TdbmTestingKernel extends Kernel
 
     public function getCacheDir()
     {
-        return __DIR__.'/../cache/'.spl_object_hash($this);
+        return __DIR__.'/../cache/'.($this->multiDb?"multidb":"singledb");
     }
 }
