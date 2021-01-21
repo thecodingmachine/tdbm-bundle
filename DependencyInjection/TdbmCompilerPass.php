0983c37c890aaa058ea4cf78291c876e6d0733fe
Guillaume <g.alabre@thecodingmachine.com>
2021-01-21 15:22:26 +0100

    Multiple Connections: Correctly inject code generators
    Technically we do use tags and interface implementations to do this automatically

---
+++ DependencyInjection/TdbmCompilerPass.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheCodingMachine\TDBM\Bundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TdbmCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $generatorListeners = $this->taggedServicesToReferences($container->findTaggedServiceIds(TdbmExtension::TAG_GENERATOR_LISTENER));
+        $codeGeneratorListeners = $this->taggedServicesToReferences($container->findTaggedServiceIds(TdbmExtension::TAG_CODE_GENERATOR_LISTENER));
+
+        $tdbmConfigurations = array_keys($container->findTaggedServiceIds(TdbmExtension::TAG_TDBM_CONFIGURATION));
+
+        foreach ($tdbmConfigurations as $tdbmConfiguration) {
+            $configuration = $container->getDefinition($tdbmConfiguration);
+            $configuration->setArgument('$generatorListeners', $generatorListeners);
+            $configuration->setArgument('$codeGeneratorListeners', $codeGeneratorListeners);
+        }
+    }
+
+    /**
+     * @param array<string, array> $taggedServices Keys are services ids, this is the output of `ContainerBuilder::findTaggedServiceIds`
+     * @return array<Reference>
+     */
+    private function taggedServicesToReferences(array $taggedServices): array
+    {
+        return array_map(static function (string $serviceId) {
+            return new Reference($serviceId);
+        }, array_keys($taggedServices));
+    }
+}
--- TdbmBundle.php
+++ TdbmBundle.php
@@ -1,12 +1,20 @@
 <?php
 
+declare(strict_types=1);
 
 namespace TheCodingMachine\TDBM\Bundle;
 
-
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use TheCodingMachine\TDBM\Bundle\DependencyInjection\TdbmCompilerPass;
 
 class TdbmBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        $container->addCompilerPass(new TdbmCompilerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
+    }
 }
